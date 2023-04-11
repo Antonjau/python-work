e83c7c5ceaeb78899a6df324dcbd19feede55b7f
Antonjau <130472003+Antonjau@users.noreply.github.com>
2023-04-11 16:44:36 +0300

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+students=["kevin","sheldon","brian","john"]
+estates=["kasarani","exit 9","syokimau","kayole"]
+
+students={
+    "kevin":"kasarani",
+    "sheldon":"exit 9",
+    "brian":"syokimau",
+    "john":"kayole"
+}
+
+for student in students:
+    print(student)
+
+for estate in estates:
+    print(estate)
+
---
+++ loops.py
@@ -0,0 +1,5 @@
+number_of_time = 0
+while number_of_time<2:
+    print ("Hello World")
+
+    number_of_time = number_of_time + 1
